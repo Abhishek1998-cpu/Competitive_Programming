405e20b23f4c6653f2643e1dc25d72f6e4953d32
Abhishek <abhishekverma998@gmail.com>
2021-01-31 11:41:30 +0530

    New

--- Leetcode/1.py
+++ Leetcode/1.py
@@ -1,26 +1,3 @@
-# Check if two Strings Arrays are Equivalent or not
-# Leetcode
-# Day 8 - January Leetcoding Challenge
-
-class Solution:
-    def arrayStringsAreEqual(self, word1: [str], word2: [str]) -> bool:
-        word = ""
-        text = ""
-        for i in range(0, len(word1)):
-            word += word1[i]
-        for j in range(0, len(word2)):
-            text += word2[j]
-        if (word == text):
-            Ans = True
-        else:
-            Ans = False
-
-        return Ans
-
-
-A = list(input().split(" "))
-B = list(input().split(" "))
-# print(A)
-# print(B)
-X = Solution()
-print(X.arrayStringsAreEqual(A, B))
+Word Ladder - Leetcode
+DFS is not used to find the shortest distance in graph(DFS will run in Exponential time)
+We will always use BFS for finding shortest distance in graph(BFS will run in Polynomial time)
