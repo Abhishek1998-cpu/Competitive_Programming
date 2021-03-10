0b818671a132fb2b2b5c0d904eaf13cb0fb34d7b
Abhishek <abhishekverma998@gmail.com>
2021-03-10 18:30:46 +0530

    New'

---
+++ Leetcode/L_Decode_XORed_Array.py
@@ -0,0 +1,9 @@
+# Decode XORed Array
+
+
+class Solution:
+    def decode(self, encoded, first):
+        ans = [first]
+        for x in encoded:
+            ans.append(ans[-1] ^ x)
+        return ans
