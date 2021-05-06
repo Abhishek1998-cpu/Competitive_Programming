dc5db6ec46cb5933d12832832d67ac89d66ac9b0
Abhishek <abhishekverma998@gmail.com>
2021-05-06 20:52:05 +0530

    String

--- Leetcode/L_Buddy_Strings.py
+++ Leetcode/L_Buddy_Strings.py
@@ -1,26 +1,28 @@
 # Buddy Strings
+# Important Question of String
+# Asked in Google
 
 class Solution:
     def buddyStrings(self, a: str, b: str) -> bool:
+        differences = []
         len1 = len(a)
         len2 = len(b)
         if(len1 != len2):
             return False
-        prev = -1
-        curr = -1
-        count = 0
-        i = 0
-        while i < len1:
-            if(a[i] != b[i]):
-                count = count + 1
-                if(count > 2):
-                    return False
-                prev = curr
-                curr = i
-            i = i + 1
-        return (count == 2 and a[prev] == b[curr] and a[curr] == b[prev])
+        if(a == b and len(set(a)) < len(a)):
+            return True
+        for x in range(len(b)):
+            if a[x] != b[x]:
+                differences.append([a[x], b[x]])
+        # print(differences)
+        # print(differences[0])
+        # print(differences[-1][::-1])
+        if len(differences) == 2 and differences[0] == differences[-1][::-1]:
+            return True
+        return False
 
 
 X = Solution()
 print(X.buddyStrings("ab", "ba"))
 print(X.buddyStrings("aaaaaaabc", "aaaaaaacb"))
+print(X.buddyStrings("aa", "aa"))
