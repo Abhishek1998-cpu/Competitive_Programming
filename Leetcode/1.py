69bcea2edf2339ce2125580bae892d81f8ecee5c
Abhishek <abhishekverma998@gmail.com>
2021-06-16 16:07:55 +0530

    Test

--- Leetcode/1.py
+++ Leetcode/1.py
@@ -1,13 +1,1 @@
-# Check if the sentence is Pangram
-# Pangram - A pangram is a sentence where every letter of the English alphabet appears at least once.
-
-class Solution:
-    def checkIfPangram(self, sentence: str) -> bool:
-        # print(len(sentence))
-        # print(set(sentence))
-        if(len(sentence) >= 26):
-            sentence = set(sentence)
-            if(len(sentence) == 26):
-                return True
-        else:
-            return False
+Test
