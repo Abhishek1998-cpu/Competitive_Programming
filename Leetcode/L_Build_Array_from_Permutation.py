f70d594f2e1c49b5e5a0beb3af1612a91843eeda
Abhishek <abhishekverma998@gmail.com>
2021-07-07 13:50:40 +0530

    Array

--- Leetcode/L_Build_Array_from_Permutation.py
+++ Leetcode/L_Build_Array_from_Permutation.py
@@ -1,10 +1,17 @@
 # Build Array from Permutation
 
+# Solution 1
+# class Solution:
+#     def buildArray(self, nums):
+#         print(nums)
+#         ans = []
+#         for i in range(0, len(nums)):
+#             ans.append(nums[nums[i]])
+#         return ans
 
+
+# Solution 2
 class Solution:
     def buildArray(self, nums):
-        print(nums)
-        ans = []
-        for i in range(0, len(nums)):
-            ans.append(nums[nums[i]])
-        return ans
+        arr = [nums[x] for x in nums]
+        return arr
