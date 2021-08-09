263e0b5940130d3afad7f6705c36d7aa4bd18aa7
Abhishek <abhishekverma998@gmail.com>
2021-08-09 12:32:28 +0530

    Math

--- Leetcode/L_Number_of_Steps_to_Reduce_a_Number_to_Zero.py
+++ Leetcode/L_Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -8,10 +8,9 @@ class Solution:
         while(num > 0):
             if((num % 2) == 0):
                 num = num // 2
-                counter += 1
             else:
                 num = num - 1
-                counter += 1
+            counter += 1
         return counter
 
 
