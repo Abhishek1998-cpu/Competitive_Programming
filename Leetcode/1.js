2b41d6c845b9aebe9ff2b8ed7d7ff93079a6cf00
Abhishek <abhishekverma998@gmail.com>
2021-10-10 00:25:49 +0530

    Array Sorting

--- Leetcode/1.js
+++ Leetcode/1.js
@@ -6,12 +6,8 @@
  * @return {number}
  */
 var maxProductDifference = function (nums) {
-  var ans = 0;
   nums.sort();
-  X = nums.length;
-  ans = nums[X - 1] * nums[X - 2] - nums[0] * nums[1];
-  return ans;
+  return nums;
 };
 
 console.log(maxProductDifference([5, 6, 2, 7, 4]));
-console.log(maxProductDifference([4, 2, 5, 9, 7, 4, 8]));
