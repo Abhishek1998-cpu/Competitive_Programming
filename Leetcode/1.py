d5a3ba5e35a0ba7a22ca8765ea3770339866cf46
Abhishek <abhishekverma998@gmail.com>
2021-10-09 12:56:11 +0530

    Master branch Solution using JavaScript

--- Leetcode/1.py
+++ Leetcode/1.py
@@ -1,3 +1,22 @@
-print("Hello Welcome to the git repo Welcome to the XBranch Hello guys Welcome to the Git")
-print("Hello Welcome to VSCode Git UI")
-print("Quanteon Developer")
+# Count Number of Pairs With Absolute Difference K
+# Array
+
+class Solution(object):
+    def countKDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        counter = 0
+        for i in range(0, len(nums)):
+            for j in range(0, len(nums)):
+                if((i < j) and (abs(nums[i] - nums[j]) == k)):
+                    counter += 1
+        return counter
+
+
+X = Solution()
+print(X.countKDifference([1, 2, 2, 1], 1))
+print(X.countKDifference([1, 3], 3))
+print(X.countKDifference([3, 2, 1, 5, 4], 2))
