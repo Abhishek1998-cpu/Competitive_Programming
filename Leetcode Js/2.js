1c2c840713416d3d2543bea47a93c6f2c95f6c57
Abhishek <abhishekverma998@gmail.com>
2022-04-13 22:42:53 +0530

    New Changes

--- Leetcode Js/2.js
+++ Leetcode Js/2.js
@@ -2,4 +2,5 @@
 // console.log(a.toString().split("."));
 // console.log(a.toString().split(".")[0]); ///before
 // console.log(a.toString().split(".")[1]); ///after
-console.log(37 / 2);
+// console.log(37 / 2);
+console.log([1,2,3,4,5].length);
