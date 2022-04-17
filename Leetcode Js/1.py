744a88091b5f79a1c5188f221e49b9b9407fcf01
Abhishek Verma <abhishek.verma@quanteon.in>
2022-04-17 17:55:12 +0530

    Palindrome Substring

--- Leetcode Js/1.py
+++ Leetcode Js/1.py
@@ -1,22 +1,5 @@
-# Checks for the largest common prefix
-def lcp(s, t):
-    n = min(len(s), len(t))
-    for i in range(0, n):
-        if(s[i] != t[i]):
-            return s[0:i]
-    else:
-        return s[0:n]
+firstName = "Abhishek"
+def myName():
+    print(firstName)
 
-
-str = "012012012012012012"
-lrs = ""
-n = len(str)
-for i in range(0, n):
-    for j in range(i+1, n):
-        # Checks for the largest common factors in every substring
-        x = lcp(str[i:n], str[j:n])
-        # If the current prefix is greater than previous one
-        # then it takes the current one as longest repeating sequence
-        if(len(x) > len(lrs)):
-            lrs = x
-print("Longest repeating sequence: "+lrs)
+myName()
--- Leetcode Js/2.js
+++ Leetcode Js/2.js
@@ -1,6 +1,33 @@
-// var a = 12345.67;
-// console.log(a.toString().split("."));
-// console.log(a.toString().split(".")[0]); ///before
-// console.log(a.toString().split(".")[1]); ///after
-// console.log(37 / 2);
-console.log([1,2,3,4,5].length);
+//Javascript program to print all possible
+// substrings of a given string
+
+// Function to print all sub strings
+function subString(str, n) {
+  // Pick starting point
+  for (let len = 1; len <= n; len++) {
+    // Pick ending point
+    for (let i = 0; i <= n - len; i++) {
+      //  Print characters from current
+      // starting point to current ending
+      // point.
+      let j = i + len - 1;
+      var s = "";
+      for (let k = i; k <= j; k++) {
+        // document.write(str[k]);
+        // console.log(str[k]);
+        s = s + str[k];
+      }
+
+      //   console.log("<br>");
+      console.log(s);
+    }
+  }
+}
+// Driver program to test above function
+let str = ["a", "b", "c", "d"];
+// let str = ["b", "a", "b", "a", "d"];
+subString(str, str.length);
+
+// This code is contributed by patel2127
+
+// subString("babad", 5);
