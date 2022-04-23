c61090c96987fa921cf138d1702aac8a9745a3d3
Abhishek Verma <abhishekverma998@gmail.com>
2022-04-23 17:43:15 +0530

    Array Greedy

--- Leetcode Js/L_Subsets.js
+++ Leetcode Js/L_Subsets.js
@@ -19,3 +19,20 @@
 };
 
 console.log(subsets([1,2,3]))
+
+
+// Recursive Solution 
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+//  var subsets = function(nums, depth = 0, subset = [], results = []) {
+//     if(depth === nums.length){
+//         results.push(subset)
+//     }else{
+//         subsets(nums, depth + 1, subset, results)
+//         subsets(nums, depth + 1, [...subset, nums[depth]], results)
+//     }
+//     return results
+// };
